4b84d5dad4a2b6a497ee2b0d37b7b10ec600c492
Sengho825 <53143098+Sengho825@users.noreply.github.com>
2022-09-05 22:36:53 +0700

    deploying

--- country_list/deploy.sh
+++ country_list/deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+#git push -f git@github.com:Sengho825/counttrylist.git master:ghpages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:Sengho825/countrylist.git master:gh-pages
 
 cd -
--- country_list/vue.config.js
+++ country_list/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/countrylist" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/counttrylist" : "/",
   transpileDependencies: [
     'vuetify'
   ],
